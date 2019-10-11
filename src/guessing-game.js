728405f48c08c052c2c3820d7b66fb4485aeba2e
oleksandr-yehorov <sasha_yegorov@ukr.net>
2019-10-12 00:06:50 +0300

    feat: add comments

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,28 @@
 class GuessingGame {
     constructor() { }
 
+    // Setting minimum and maximum range value
+
     setRange(min, max) {
         this.min = min;
         this.max = max;
     }
+
+    // Guess function returns the lowest integer, greater
+    // than average of min and max range
 
     guess() {
         this.value = Math.ceil((this.min + this.max) / 2);
         return this.value;
     }
 
+    // Set a new maximum value
+
     lower() {
         this.max = this.value;
     }
+
+    // Set a new minimum value
 
     greater() {
         this.min = this.value;
